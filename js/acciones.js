40b84d7b68983a09cebbea46f45267cb4756d278
rodrigoDoradoGtz <rodrigodgtz3f.desaupoo@gmail.com>
2016-05-17 14:15:49 -0500

    5tasubida

--- js/acciones.js
+++ js/acciones.js
@@ -26,24 +26,24 @@ $(document).ready(function(e) {
 }
 function Correcto(acceleration) { 
 var element = document.getElementById('acelerometro'); 
-element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br />' +
- 'Aceleracion en Y: ' + acceleration.y + '<br />' +
- 'Aceleracion en Z: ' + acceleration.z + '<br />' +
- 'Intervalo: ' + acceleration.timestamp + '<br />' 
+element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br/>' +
+ 'Aceleracion en Y: ' + acceleration.y + '<br/>' +
+ 'Aceleracion en Z: ' + acceleration.z + '<br/>' +
+ 'Intervalo: ' + acceleration.timestamp + '<br/>' 
 } 
 function Error(){
 	alert('Error!')
 }
 function Localiza(posicion) { 
 var element = document.getElementById('geolocalizacion');
- element.innerHTML = 'Latitude: '  + posicion.coords.latitude + '<br />' + 
-'Longitud: ' + posicion.coords.longitude  + '<br />' + 
-'Altitud: ' + posicion.coords.altitude + '<br />' + 
+ element.innerHTML = 'Latitude: '  + posicion.coords.latitude + '<br/>' + 
+'Longitud: ' + posicion.coords.longitude  + '<br/>' + 
+'Altitud: ' + posicion.coords.altitude + '<br/>' + 
 'Precision: ' + posicion.coords.accuracy + '<br />' + 
-'Precision de Altitud: ' + posicion.coords.altitudeAccuracy + '<br />' + 
-'Direccion: ' + posicion.coords.heading + '<br />' + 
-'Velocidad: ' + posicion.coords.speed + '<br />' + 
-'Intervalo: ' + posicion.timestamp + '<br />'; 
+'Precision de Altitud: ' + posicion.coords.altitudeAccuracy + '<br/>' + 
+'Direccion: ' + posicion.coords.heading + '<br/>' + 
+'Velocidad: ' + posicion.coords.speed + '<br/>' + 
+'Intervalo: ' + posicion.timestamp + '<br/>'; 
 } 
 function ErrorLocalizacion(error){
 	alert('codigo:' +error.code +'\n'+
