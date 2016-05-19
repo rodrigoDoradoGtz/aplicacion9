c135f88b21a0e93c95a4aa387a0e61254ea031af
rodrigoDoradoGtz <rodrigodgtz3f.desaupoo@gmail.com>
2016-05-19 14:46:55 -0500

    7tasubida

--- js/acciones.js
+++ js/acciones.js
@@ -1,51 +1,18 @@
 // JavaScript Document
 $(document).ready(function(e) {
-    //watchID se refiere a la aceleracion 'actual'
-	//
-	var watchID=null;
-	document.addEventListener("deviceready", Dispositivo_Listo, false);
-	//cuando esta listo el dipositivo
-	//
-	function Dispositivo_Listo(){
-		Comienza();
+    document.addEventListener("deviceready", onDeviceReady, false);
+	function onDeviceReady() {
+		$('#localizar').on('tap', function(){
+			navigator.geolocation.getCurrentPosition(function (position){
+				$('#localizacion').html('Latitude: '        + position.coords.latitude       + '\n' +
+				'Longitude: '          + position.coords.longitude        + '\n' +
+				'Altitude: '           + position.coords.altitude         + '\n' +
+				'Accuracy: '           + position.coords.accuracy         + '\n' +
+				'Altitude Accuracy: '  + position.coords.altitudeAccuracy + '\n' +
+				'Heading: '            + position.coords.heading          + '\n' +
+				'Speed: '              + position.coords.speed            + '\n' +
+				'Timestamp: '          + position.timestamp               + '\n' );
+			}, $('#localizacion').html('Error en la localizacion'));
+		});
 	}
-	//empieza la 'observacion' de la aceleracion
-	//
-	function Comienza(){
-		var Opciones={frequency:200};
-		watchID=navigator.accelerometer.watchAcceleration(Correcto, Error, Opciones);
-		navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
-	}
-	function Detente() { 
-     if (watchID) { 
-      navigator.accelerometer.clearWatch(watchID); 
-      watchID = null; 
-
-    }
-	}
-function Correcto(acceleration) { 
-var element = document.getElementById('acelerometro'); 
-element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br/>' +
- 'Aceleracion en Y: ' + acceleration.y + '<br/>' +
- 'Aceleracion en Z: ' + acceleration.z + '<br/>' +
- 'Intervalo: ' + acceleration.timestamp + '<br/>' 
-} 
-function Error(){
-	alert('Error!')
-}
-function Localiza(posicion) { 
-var element = document.getElementById('geolocalizacion');
- element.innerHTML = 'Latitude: '  + posicion.coords.latitude + '<br/>' + 
-'Longitud: ' + posicion.coords.longitude  + '<br/>' + 
-'Altitud: ' + posicion.coords.altitude + '<br/>' + 
-'Precision: ' + posicion.coords.accuracy + '<br />' + 
-'Precision de Altitud: ' + posicion.coords.altitudeAccuracy + '<br/>' + 
-'Direccion: ' + posicion.coords.heading + '<br/>' + 
-'Velocidad: ' + posicion.coords.speed + '<br/>' + 
-'Intervalo: ' + posicion.timestamp + '<br/>'; 
-} 
-function ErrorLocalizacion(error){
-	alert('codigo:' +error.code +'\n'+
-	'mensaje:' +error.message + '\n');
-}
 });
